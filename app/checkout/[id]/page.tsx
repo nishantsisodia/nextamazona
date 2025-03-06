8b7362359f1998c8408fb6eeda4a169d7ba3bb2b
nishantsisodia <nishurajput68malikpur@gmail.com>
2025-03-06 18:29:48 +0530

    add new commit

--- app/checkout/[id]/page.tsx
+++ app/checkout/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from 'next/navigation'
 import React from 'react'
+
 import { auth } from '@/auth'
 import { getOrderById } from '@/lib/actions/order.actions'
 import PaymentForm from './payment-form'
@@ -24,22 +25,20 @@ const CheckoutPaymentPage = async (props: {
   const session = await auth()
 
   let client_secret = null
-if (order.paymentMethod === 'Stripe' && !order.isPaid) {
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: Math.round(order.totalPrice * 100),
-    currency: 'USD',
-    metadata: { orderId: order._id },
-  })
-  client_secret = paymentIntent.client_secret
-}
-
-
+  if (order.paymentMethod === 'Stripe' && !order.isPaid) {
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(order.totalPrice * 100),
+      currency: 'USD',
+      metadata: { orderId: order._id },
+    })
+    client_secret = paymentIntent.client_secret
+  }
   return (
     <PaymentForm
       order={order}
       paypalClientId={process.env.PAYPAL_CLIENT_ID || 'sb'}
-      clientSecret = { client_secret }
+      clientSecret={client_secret}
       isAdmin={session?.user?.role === 'Admin' || false}
     />
   )

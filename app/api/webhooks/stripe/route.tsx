c57fc36a02a20b0422844c64c0a4f5a0747087af
nishantsisodia <nishurajput68malikpur@gmail.com>
2025-03-06 19:08:01 +0530

    sdfasdf commit

--- app/api/webhooks/stripe/route.tsx
+++ app/api/webhooks/stripe/route.tsx
@@ -1,57 +1,45 @@
-// Webhook route file (e.g., /api/webhooks/stripe.ts)
-
 import { NextRequest, NextResponse } from 'next/server'
 import Stripe from 'stripe'
 
 import { sendPurchaseReceipt } from '@/emails'
 import Order from '@/lib/db/models/order.model'
 
-// Stripe instance
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
-export const config = {
-  api: {
-    bodyParser: false, // Disable body parsing for this API route
-  },
-}
-
 export async function POST(req: NextRequest) {
-  try {
-    const event = await stripe.webhooks.constructEvent(
-      await req.text(), // Raw body for signature verification
-      req.headers.get('stripe-signature') as string,
-      process.env.STRIPE_WEBHOOK_SECRET as string
-    )
+  const event = await stripe.webhooks.constructEvent(
+    await req.text(),
+    req.headers.get('stripe-signature') as string,
+    process.env.STRIPE_WEBHOOK_SECRET as string
+  )
 
-    // Handle event type
-    if (event.type === 'charge.succeeded') {
-      const charge = event.data.object
-      const orderId = charge.metadata.orderId
-      const order = await Order.findById(orderId).populate('user', 'email')
-
-      if (order == null) {
-        return new NextResponse('Bad Request', { status: 400 })
-      }
-
-      // Update order status
-      order.isPaid = true
-      order.paidAt = new Date()
-      await order.save()
-
-      try {
-        // Send receipt email
-        await sendPurchaseReceipt({ order })
-      } catch (err) {
-        console.log('Email error', err)
-      }
-
-      return new NextResponse('Webhook processed successfully', { status: 200 })
+  if (event.type === 'charge.succeeded') {
+    const charge = event.data.object
+    const orderId = charge.metadata.orderId
+    const email = charge.billing_details.email
+    const pricePaidInCents = charge.amount
+    const order = await Order.findById(orderId).populate('user', 'email')
+    if (order == null) {
+      return new NextResponse('Bad Request', { status: 400 })
     }
 
-    // If the event type is not handled, return no content
-    return new NextResponse('Webhook event type not handled', { status: 200 })
-  } catch (err) {
-    console.error('Webhook Error:', err)
-    return new NextResponse('Webhook Error', { status: 500 })
+    order.isPaid = true
+    order.paidAt = new Date()
+    order.paymentResult = {
+      id: event.id,
+      status: 'COMPLETED',
+      email_address: email!,
+      pricePaid: (pricePaidInCents / 100).toFixed(2),
+    }
+    await order.save()
+    try {
+      await sendPurchaseReceipt({ order })
+    } catch (err) {
+      console.log('email error', err)
+    }
+    return NextResponse.json({
+      message: 'updateOrderToPaid was successful',
+    })
   }
+  return new NextResponse()
 }

3070d0d1f01d919493aed4435e1bed24adc6a75b
nishantsisodia <nishurajput68malikpur@gmail.com>
2025-03-08 19:35:20 +0530

    add new commits

--- app/api/webhooks/stripe/route.tsx
+++ app/api/webhooks/stripe/route.tsx
@@ -7,37 +7,43 @@ import Order from '@/lib/db/models/order.model'
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
 export async function POST(req: NextRequest) {
-  const event = await stripe.webhooks.constructEvent(
-    await req.text(),
-    req.headers.get('stripe-signature') as string,
-    process.env.STRIPE_WEBHOOK_SECRET as string
-  )
+  try {
+    const event = await stripe.webhooks.constructEvent(
+      await req.text(),
+      req.headers.get('stripe-signature') as string,
+      process.env.STRIPE_WEBHOOK_SECRET as string
+    )
 
-  if (event.type === 'charge.succeeded') {
-    const charge = event.data.object
-    const orderId = charge.metadata.orderId
-    const order = await Order.findById(orderId).populate('user', 'email')
-    if (order == null) {
-      return new NextResponse('Bad Request', { status: 400 })
-    }
+    if (event.type === 'charge.succeeded') {
+      const charge = event.data.object
+      const orderId = charge.metadata.orderId
+      const order = await Order.findById(orderId).populate('user', 'email')
+      if (order == null) {
+        return new NextResponse('Bad Request', { status: 400 })
+      }
 
-    order.isPaid = true
-    order.paidAt = new Date()
-    order.paymentResult = {
-      id: event.id,
-      status: 'COMPLETED',
-      email_address: charge.billing_details.email!,
-      pricePaid: (charge.amount / 100).toFixed(2),
+      order.isPaid = true
+      order.paidAt = new Date()
+      order.paymentResult = {
+        id: event.id,
+        status: 'COMPLETED',
+        email_address: charge.billing_details.email!,
+        pricePaid: (charge.amount / 100).toFixed(2),
+      }
+      await order.save()
+      try {
+        await sendPurchaseReceipt({ order })
+      } catch (err) {
+        console.error('Error sending email', err)
+      }
+      return NextResponse.json({
+        message: 'updateOrderToPaid was successful',
+      })
     }
-    await order.save()
-    try {
-      await sendPurchaseReceipt({ order })
-    } catch (err) {
-      console.log('email error', err)
-    }
-    return NextResponse.json({
-      message: 'updateOrderToPaid was successful',
-    })
+    return new NextResponse('Event type not handled', { status: 400 })
+  } catch (error) {
+    console.error('Error processing Stripe webhook', error)
+    return new NextResponse('Internal Server Error', { status: 500 })
   }
-  return new NextResponse()
 }
+
